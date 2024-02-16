87f111fa2d469a8728eaf164d18e3a33d226a50c
Mori0000 <sd21092@tti-j.net>
2024-02-16 16:45:51 +0900

    2

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+        <div className='logo'>
+            <h3>ポートフォリオ</h3>
+        </div>
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/blog'>Blog</Link></li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Button>Home</Button>
+    </div>
+  )
+}
+
+export default HomePage
